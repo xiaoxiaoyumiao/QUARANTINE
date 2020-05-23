caed6af2a50d673b6af496fbf04c2bb047c42e59
yuc17 <yuc17@mails.tsinghua.edu.cn>
2020-05-23 12:24:59 +0800

    line rendered

--- Assets/Scripts/LinePainter.cs
+++ Assets/Scripts/LinePainter.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LinePainter : MonoBehaviour
 {
+    public Material material;
+    public Color lineColor;
+    public float lineSize = 1f;
+
     Landscape landscape;
     List<GameObject> landblocks;
 
@@ -42,11 +46,43 @@ public class LinePainter : MonoBehaviour
         }
     }
 
+    List<GameObject> lines = new List<GameObject>();
+
+    public void PaintLine(Vector3 start, Vector3 end)
+    {
+        GameObject line = new GameObject();
+        lines.Add(line);
+        line.transform.parent = transform;
+        myRenderer = line.AddComponent<LineRenderer>();
+
+        myRenderer.material = material;
+        myRenderer.startColor = lineColor;
+        myRenderer.endColor = lineColor;
+
+        myRenderer.startWidth = lineSize;
+        myRenderer.endWidth = lineSize;
+
+        myRenderer.positionCount = 2;
+        myRenderer.SetPosition(0, start);
+        myRenderer.SetPosition(1, end);
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        GameObject line = new GameObject();
-        line.transform.parent = transform;
-        myRenderer = line.AddComponent<LineRenderer>();
+       foreach (var ele in lines)
+        {
+            Destroy(ele);
+        }
+       foreach (var block in Blocks)
+        {
+            Vector3 start = block.blockUI.transform.position;
+            foreach (var outb in block.outBlocks)
+            {
+                Vector3 end = outb.blockUI.transform.position;
+                PaintLine(start, end);
+            }
+        }
     }
 }
